249abdd985859bdc5eaad9232c02554cd8963357
ItryCode <sonuhrajeev@gmail.com>
2023-11-09 12:56:51 +0530

    refactoring

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -1,6 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
 import customInstance from "../../utils/axios";
+import { getAllJobsThunk, showStatsThunk } from "./allJobsThunk";
 
 const initialFiltersState = {
   search: "",
@@ -21,41 +22,9 @@ const initialState = {
   ...initialFiltersState,
 };
 
-export const getAllJobs = createAsyncThunk(
-  "allJobs/get",
-  async (_, thunkAPI) => {
-    const { page, search, searchStatus, searchType, sort } =
-      thunkAPI.getState().allJobs;
-    let url = `/jobs?status=${searchStatus}&jobType=${searchType}&sort=${sort}&page=${page}`;
-    if (search) {
-      url = url + `&search=${search}`;
-    }
-    try {
-      const resp = await customInstance(url, {
-        headers: {
-          authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
-        },
-      });
+export const getAllJobs = createAsyncThunk("allJobs/getJobs", getAllJobsThunk);
 
-      return resp.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue("There was an error..");
-    }
-  }
-);
-
-export const showStats = createAsyncThunk(
-  "allJobs/showStats",
-  async (_, thunkAPI) => {
-    try {
-      const resp = await customInstance("/jobs/stats");
-      console.log(resp.data);
-      return resp.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.response.data.msg);
-    }
-  }
-);
+export const showStats = createAsyncThunk("allJobs/showStats", showStatsThunk);
 
 const allJobsSlice = createSlice({
   name: "allJobs",
---
+++ src/features/allJobs/allJobsThunk.js
@@ -0,0 +1,31 @@
+import customInstance from "../../utils/axios";
+
+export const getAllJobsThunk = async (_, thunkAPI) => {
+  const { page, search, searchStatus, searchType, sort } =
+    thunkAPI.getState().allJobs;
+  let url = `/jobs?status=${searchStatus}&jobType=${searchType}&sort=${sort}&page=${page}`;
+  if (search) {
+    url = url + `&search=${search}`;
+  }
+  try {
+    const resp = await customInstance(url, {
+      headers: {
+        authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+      },
+    });
+
+    return resp.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue("There was an error..");
+  }
+};
+
+export const showStatsThunk = async (_, thunkAPI) => {
+  try {
+    const resp = await customInstance("/jobs/stats");
+    console.log(resp.data);
+    return resp.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response.data.msg);
+  }
+};
